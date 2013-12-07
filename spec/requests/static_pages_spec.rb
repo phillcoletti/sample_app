a5d9a22fe6ae2535d8cd19ecd71d7482a35cde90
phillcoletti <coletti.phillip@gmail.com>
2013-12-07 16:42:37 -0500

    Finish Static Pages

--- spec/requests/static_pages_spec.rb
+++ spec/requests/static_pages_spec.rb
@@ -2,10 +2,10 @@ require 'spec_helper'
 
 describe "Static pages" do
   describe "Home page" do
-    it "should have the content 'Sample_App'" do
+    it "should have the content 'Sample App'" do
       # Run the generator again with the --webrat flag if you want to use webrat methods/matchers
       visit '/static_pages/home'
-      expect(page).to have_content('Sample_App')
+      expect(page).to have_content('Sample App')
     end
     it "should have the right title" do
       visit '/static_pages/home'
@@ -19,7 +19,7 @@ describe "Static pages" do
           expect(page).to have_content('Help')
       end
       it "should have the right title" do
-      visit '/static_pages/home'
+      visit '/static_pages/help'
       expect(page).to have_title("Ruby on Rails Tutorial Sample App | Help")
     end
   end
@@ -30,7 +30,7 @@ describe "Static pages" do
           expect(page).to have_content('About Us')
       end
       it "should have the right title" do
-      visit '/static_pages/home'
+      visit '/static_pages/about'
       expect(page).to have_title("Ruby on Rails Tutorial Sample App | About Us")
     end
   end

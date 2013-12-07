a13d32e3bb99dcedee1067344a2ee627616c518a
phillcoletti <coletti.phillip@gmail.com>
2013-12-07 16:30:08 -0500

    Fixed Titles

--- spec/requests/static_pages_spec.rb
+++ spec/requests/static_pages_spec.rb
@@ -2,7 +2,7 @@ require 'spec_helper'
 
 describe "Static pages" do
   describe "Home page" do
-    it "should have the content 'Sample App'" do
+    it "should have the content 'Sample_App'" do
       # Run the generator again with the --webrat flag if you want to use webrat methods/matchers
       visit '/static_pages/home'
       expect(page).to have_content('Sample_App')
